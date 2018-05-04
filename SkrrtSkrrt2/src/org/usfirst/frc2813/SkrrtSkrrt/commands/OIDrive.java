0ceafdff7783ccbeb3b4248c336fbb12dcda0578
Grady Whelan <grady.whelan@gmail.com>
2018-05-04 14:48:58 -0700

    Initial Commit

---
+++ SkrrtSkrrt2/src/org/usfirst/frc2813/SkrrtSkrrt/commands/OIDrive.java
@@ -0,0 +1,44 @@
+package org.usfirst.frc2813.SkrrtSkrrt.commands;
+
+import org.usfirst.frc2813.SkrrtSkrrt.Robot;
+
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.command.Command;
+
+/**
+ *
+ */
+public class OIDrive extends Command {
+	private Joystick joystickLeft = Robot.oi.leftJoystick;
+	private Joystick joystickRight = Robot.oi.rightJoystick;
+    public OIDrive() {
+    	requires(Robot.driveTrain);
+        // Use requires() here to declare subsystem dependencies
+        // eg. requires(chassis);
+    }
+
+    // Called just before this Command runs the first time
+    protected void initialize() {
+    }
+
+    // Called repeatedly when this Command is scheduled to run
+    protected void execute() {
+    	Robot.driveTrain.tankDrive(joystickLeft, joystickRight);
+    	Robot.driveTrain.leftSpeed -= Robot.driveTrain.deceleration;
+    	Robot.driveTrain.rightSpeed -= Robot.driveTrain.deceleration;
+    }
+
+    // Make this return true when this Command no longer needs to run execute()
+    protected boolean isFinished() {
+        return false;
+    }
+
+    // Called once after isFinished returns true
+    protected void end() {
+    }
+
+    // Called when another command which requires one or more of the same
+    // subsystems is scheduled to run
+    protected void interrupted() {
+    }
+}
