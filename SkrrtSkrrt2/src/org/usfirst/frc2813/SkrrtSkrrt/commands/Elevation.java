a29bb360cd4873631c937634ee094d126293780b
Grady Whelan <grady.whelan@gmail.com>
2018-05-15 16:27:37 -0700

    Elevation Motors and Command

--- SkrrtSkrrt2/src/org/usfirst/frc2813/SkrrtSkrrt/commands/Elevation.java
+++ SkrrtSkrrt2/src/org/usfirst/frc2813/SkrrtSkrrt/commands/Elevation.java
@@ -2,6 +2,9 @@ package org.usfirst.frc2813.SkrrtSkrrt.commands;
 
 import org.usfirst.frc2813.SkrrtSkrrt.Direction;
 import org.usfirst.frc2813.SkrrtSkrrt.Robot;
+import org.usfirst.frc2813.SkrrtSkrrt.RobotMap;
+
+import com.ctre.phoenix.motorcontrol.can.WPI_VictorSPX;
 
 import edu.wpi.first.wpilibj.command.Command;
 
@@ -10,6 +13,8 @@ import edu.wpi.first.wpilibj.command.Command;
  */
 public class Elevation extends Command {
 	private Direction elevationDirection;
+	private WPI_VictorSPX motor;
+	
 	
     public Elevation(Direction d) {
         requires(Robot.cannonMotors);
@@ -19,11 +24,25 @@ public class Elevation extends Command {
 
     // Called just before this Command runs the first time
     protected void initialize() {
+    	switch(Robot.cannonMotors.whichSide) {
+    	case LEFT:	
+    		this.motor = RobotMap.driveTrainLeftMotor;
+    		break;
+    	case RIGHT:	
+        	this.motor = RobotMap.driveTrainLeftMotor;
+    		break;
+    	}
     }
 
     // Called repeatedly when this Command is scheduled to run
     protected void execute() {
-    	
+    	switch(elevationDirection) {
+    	case UP:
+    		motor.set(1);
+    		break;
+    	case DOWN:
+    		motor.set(-1);
+    	}
     }
 
     // Make this return true when this Command no longer needs to run execute()
@@ -33,10 +52,12 @@ public class Elevation extends Command {
 
     // Called once after isFinished returns true
     protected void end() {
+    	motor.set(0);
     }
 
     // Called when another command which requires one or more of the same
     // subsystems is scheduled to run
     protected void interrupted() {
+    	motor.set(0);
     }
 }
